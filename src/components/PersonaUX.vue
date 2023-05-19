affd908ff626a773c4e6fde4d036f81f9ed12a18
JesusAdrianHobakMartinez <jesus.18070037@itsmotul.edu.mx>
2023-05-18 20:30:15 -0600

    prueva 5

--- src/components/PersonaUX.vue
+++ src/components/PersonaUX.vue
@@ -264,7 +264,7 @@ export default {
                     <div class="md:w-1/3"></div>
                     <div class="md:w-2/3">
                         <div @click="Enviar()">
-                            <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                            <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer ..." type="button " >
                                 Enviar
                             </button>
                         </div>
